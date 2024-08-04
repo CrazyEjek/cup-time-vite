753c4f565f75bd8afaa150b7bf48e2ae74b11aa6
CrazyEjek <myejek@gmail.com>
2024-08-04 18:52:17 +0500

    less 4_2

--- src/modules/Order.jsx
+++ src/modules/Order.jsx
@@ -1,5 +1,54 @@
-export const Order = () => {
-    <section className="order">order
+export const Order = () => (
+    <section className="order">
+        <div className="container">
+            <h2 className="order__title">Доставка</h2>
+
+            <form action="" className="order__form">
+                <input 
+                type="text" 
+                className="order__input" 
+                name="name" 
+                placeholder="Имя" />
+
+                <input 
+                type="text" 
+                className="order__input" 
+                name="phone" 
+                placeholder="Телефон" />
+
+                <input 
+                type="text" 
+                className="order__input order__input_address" 
+                name="address" 
+                placeholder="Адрес" />
+
+                <fieldset className="order__payment">
+                <legend className="order__payment-title">Оплата:</legend>
+      
+                  <label 
+                  className="order__payment-label">
+                    <input 
+                    className="order__radio" 
+                    type="radio" 
+                    name="payment" 
+                    value="cash" 
+                    defaultChecked/>
+                    Наличными
+                  </label>
+      
+                  <label className="order__payment-label">
+                    <input 
+                    className="order__radio" 
+                    type="radio" 
+                    name="payment" 
+                    value="credit card"/>
+                    Картой
+                  </label>
+                
+              </fieldset>
+
+            </form>
+        </div>
         
     </section>
-}
+);
--- src/modules/Products.jsx
+++ src/modules/Products.jsx
@@ -1,42 +1,6 @@
 import { Product } from "./Product";
+import { products } from "./products";
 
-const products = [
-    {   id: 1,
-        title:"Зеленый индонезийский чай",
-        image:"/image/photo-1.jpg",
-        price:390,
-    },
-
-    {   id: 2,
-        title:"Кокосовая матча",
-        image:"/image/photo-2.jpg",
-        price:380,
-    },
-
-    {   id: 3,
-        title:"Чай черный Лакандона",
-        image:"/image/photo-3.jpg",
-        price:450,
-    },
-
-    {   id: 4,
-        title:"Органическая веганская матча",
-        image:"/image/photo-4.jpg",
-        price:470,
-    },
-
-    {   id: 5,
-        title:"Черный чай из Калифорнии",
-        image:"/image/photo-5.jpg",
-        price:360,
-    },
-
-    {   id: 6,
-        title:"Черный чай из Эфиопии",
-        image:"/image/photo.jpg",
-        price:300,
-    },
-];
 
 export const Products = () => {
     return (
